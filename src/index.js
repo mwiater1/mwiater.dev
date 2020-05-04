5d2aa6b68fb98e57b11e54f9fea8d217dd6b34a1
Mateusz Wiater <mateuszwiater@gmail.com>
2020-05-03 22:27:34 -0500

    Set up coming soon website.

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import "jquery";
+import "popper.js";
+import "bootstrap/dist/js/bootstrap.min"
+import './style.scss';
+// eslint-disable-next-line import/no-unresolved,import/no-webpack-loader-syntax
+import "expose-loader?THREE!three";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Index from './pages';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Index/>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
---
+++ src/pages/index.js
@@ -0,0 +1,44 @@
+import React, {useRef, useEffect} from 'react';
+import styled from 'styled-components';
+import HALO from 'vanta/src/vanta.halo';
+import classNames from "classnames";
+
+const Container = styled.div`
+  min-height: 100vh;
+`;
+
+const Heading = styled(({className, ...rest}) => (
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        <p className={classNames('display-1', className)} {...rest}/>
+    )
+)`
+  font-size: ${props => `${props.size}px`};
+`;
+
+const Index = () => {
+    const ref = useRef(null);
+
+    useEffect(() => {
+        const vanta = HALO({
+            el: ref.current,
+            xOffset: 0.25
+        });
+
+        return () => vanta.destroy();
+    }, []);
+
+    return (
+        <div ref={ref}>
+            <Container className="container d-flex flex-column justify-content-center">
+                <div className="row">
+                    <div className="col text-white">
+                        <Heading size={72}>Mateusz Wiater</Heading>
+                        <Heading size={24}>New website coming soon!</Heading>
+                    </div>
+                </div>
+            </Container>
+        </div>
+    );
+};
+
+export default Index;
